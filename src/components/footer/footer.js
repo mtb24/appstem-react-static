bea21a3a5f88d1e69128b840e6378036732026e5
mtb24 <kendowney.com@gmail.com>
2018-04-21 14:29:50 -0700

    add footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-static'
+import Nav from '../nav/nav'
+
+import './footer.css'
+
+export default () => (
+    <div className={ 'footer-wrapper' }>
+        <Nav />
+    </div>
+)
