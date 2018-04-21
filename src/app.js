ab709e5f43f355a6cd6afe092aa97eeb0b4330c1
mtb24 <kendowney.com@gmail.com>
2018-04-21 14:26:28 -0700

    adds app

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Router, Link } from 'react-static'
+import { hot } from 'react-hot-loader'
+import Routes from 'react-static-routes'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+
+import './app.css'
+
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+      <div className="content">
+        <Routes />
+      </div>
+      <Footer />
+    </div>
+  </Router>
+)
+
+export default hot(module)(App)
