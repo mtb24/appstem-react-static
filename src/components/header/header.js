b6b635cd62660aa0e71b9fd33fbf1ad5a895eea3
mtb24 <kendowney.com@gmail.com>
2018-04-25 12:54:44 -0700

    add header logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-static'
 import Nav from '../nav/nav'
-
+import AppstemLogo from '../social/icons/appstem-logo'
 import './header.css'
 
 export default () => (
     <div className={ 'header-wrapper' }>
-        <Link to="/">Appstem</Link>
+        <Link to="/"><AppstemLogo className='header-logo' width='180' height='34' fillColor='#ffffff'/></Link>
         <Nav />
     </div>
 );
