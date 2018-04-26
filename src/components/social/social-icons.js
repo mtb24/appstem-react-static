ed37029b4d70908061f2dc2e40aec2175feb71fd
mtb24 <kendowney.com@gmail.com>
2018-04-25 23:54:46 -0700

    formatting

--- src/components/social/social-icons.js
+++ src/components/social/social-icons.js
@@ -10,37 +10,50 @@ import './social-icons.css'
 export default () => (
 
     <div className={ 'social-icons-wrapper' }>
+
         <Link to='https://twitter.com/appstem'>  
+
             <TwitterIcon
                 fillColor='#fefefe'
                 className='social-icon-twitter'
                 width='40'
                 height='40'
             />
+
         </Link>
-        <Link to='https://www.facebook.com/appstemmedia'>        
+
+        <Link to='https://www.facebook.com/appstemmedia'>
+
             <FacebookIcon
                 fillColor='#fefefe'
                 className='social-icon-facebook'
                 width='40'
                 height='40'
             />
+
         </Link>
+
         <Link to='https://www.instagram.com/appstem'>
+
             <InstagramIcon
                 fillColor='#fefefe'
                 className='social-icon-instagram'
                 width='40'
                 height='40'
             />
+
         </Link>
+
         <Link to='https://medium.com/appstem'>
+        
             <MediumIcon
                 fillColor='#fefefe'
                 className='social-icon-medium'
                 width='40'
                 height='40'
             />
+            
         </Link>
+
     </div>
 )
