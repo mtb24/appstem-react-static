0b36185d2dd9ca19eb90bca43bf1457ce2c5847a
mtb24 <kendowney.com@gmail.com>
2018-05-01 09:38:47 -0700

    add hero background image

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,12 +1,13 @@
-
 import React from 'react'
 import { withRouteData } from 'react-static'
 import convert from 'htmr'
-
+import Hero from '../hero/hero'
 import './contact.css'
+import backgroundImage from './contact-background.png'
 
 export default withRouteData(({ contact }) => (
-  <div>
+  <div className='contact-wrapper'>
+    <Hero title={ contact.title } backgroundImage={ backgroundImage } />
     {convert(contact.contents)}
   </div>
 ))
