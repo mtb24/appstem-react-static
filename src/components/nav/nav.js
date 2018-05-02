1fa71409527068db5bef531e54b058dae959d75c
mtb24 <kendowney.com@gmail.com>
2018-05-02 00:55:42 -0700

    add wrapper class

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -5,14 +5,14 @@ import Hamburger from './hamburger'
 import './nav.css'
 
 export default (props) => (
-    <div>
-    <Hamburger />
-    <nav className={props.className}>
-        <Link to="/work">Work</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/culture">Culture</Link>
-        <Link to="/#">Blog</Link>
-        <Link to="/contact">Contact</Link>
-    </nav>
+    <div className='nav-wrapper'>
+        <Hamburger />
+        <nav className={props.className}>
+            <Link to="/work">Work</Link>
+            <Link to="/services">Services</Link>
+            <Link to="/culture">Culture</Link>
+            <Link to="/#">Blog</Link>
+            <Link to="/contact">Contact</Link>
+        </nav>
     </div>
 );
