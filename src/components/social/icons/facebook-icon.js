8e95a9a5f9166d5a18b1b1fdb89c00d885425162
mtb24 <kendowney.com@gmail.com>
2018-05-02 00:30:40 -0700

    change viewBox size

--- src/components/social/icons/facebook-icon.js
+++ src/components/social/icons/facebook-icon.js
@@ -4,7 +4,7 @@ export default props => {
     const fillcolor = props.fillColor || 'cadetblue'
 
     return (
-        <svg id="facebook-icon" className={ props.className } viewBox={`0 0 ${props.width} ${props.height}`} width={props.width} height={props.height}  aria-labelledby="title" xmlns="http://www.w3.org/2000/svg">
+        <svg id="facebook-icon" className={ props.className } viewBox={'0 0 40 40'} width={props.width} height={props.height}  aria-labelledby="title" xmlns="http://www.w3.org/2000/svg">
             <title id='title'>Link to our Facebook page</title>
             <path fill={fillcolor} d="M22.842 35.15V20.94h4.925l.737-5.537h-5.662v-3.535c0-1.603.46-2.696 2.834-2.696h3.028V4.217C28.18 4.15 26.384 4 24.292 4c-4.366 0-7.355 2.58-7.355 7.32v4.083H12v5.538h4.937v14.21h5.905z"/>
         </svg>
