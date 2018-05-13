2a57f47b3d8dc7e80dd0b56c8131510213ce9f99
mtb24 <kendowney.com@gmail.com>
2018-05-13 13:28:26 -0700

    add FeatureBlock component

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { withRouteData, Link } from 'react-static'
 import Hero from '../hero/hero'
+import Section from '../layout/page-section/section'
 import backgroundImage from './work-background.png'
 import './work.css'
+import FeatureBlock from '../feature-block/feature-block';
 
 export default withRouteData(({ work }) => (
   <div className='work-wrapper'>
     <Hero title='Our Work' backgroundImage={ backgroundImage } />
-
-    <ul>
-      {work.map(caseStudy => (
-        <li key={caseStudy.slug}>
-          <Link to={`/work/${caseStudy.slug}/`}>{caseStudy.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <Section className='mask-slope-top-right'>
+      <ul>
+        {work.map( (caseStudy, i) => {
+          let imagePosition = ( i % 2 == 0 )? 'left' : 'right'
+          return (
+          <li key={caseStudy.slug}>
+            <Link to={`/work/${caseStudy.slug}/`}>
+              <FeatureBlock imagePosition={imagePosition} caseStudy={caseStudy} />
+            </Link>
+          </li>
+        )})}
+      </ul>
+    </Section>
   </div>
 ))
