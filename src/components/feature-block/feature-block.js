497f325a9e1f59b40ff0c5291086e1ea9d623596
mtb24 <kendowney.com@gmail.com>
2018-05-13 13:23:11 -0700

    add FeatureBlock component

---
+++ src/components/feature-block/feature-block.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import convert from 'htmr'
+import { Link } from 'react-static'
+import Arrow from './arrow_white.png'
+
+import './feature-block.css'
+
+export default props => {
+
+    return (
+    <div className={`feature-block-wrapper feature-image-${props.imagePosition}`}>
+        { !props.caseStudy.imageURL ? null: (<img src={props.caseStudy.imageURL} className='feature-block-image' alt={props.caseStudy.imageAlt} />) }
+        <div className='feature-block-details'>
+            <h4>{ props.caseStudy.title }</h4>
+            <p>{ props.caseStudy.type }</p>
+            <p>{ convert(props.caseStudy.contents) }</p>
+        </div>
+        <Link to='#'>View the case study <img src={Arrow} alt='White arrow icon' /></Link>
+    </div>
+)}
