7464ead1f4f4c69db30a60ae9b4ba8af8b652114
mtb24 <kendowney.com@gmail.com>
2018-05-17 03:01:13 -0700

    add testimonials

--- src/components/culture/culture.js
+++ src/components/culture/culture.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { withRouteData } from 'react-static'
 import convert from 'htmr'
 import Hero from '../hero/hero'
+import Testimonials from '../testimonials/testimonials'
 import backgroundImage from './culture-background.png'
 
 import './culture.scss'
 
-export default withRouteData(({ culture }) => (
+export default withRouteData(({ culture, testimonials }) => (
+
   <div className='culture-wrapper'>
     <Hero title='Culture' backgroundImage={ backgroundImage } />
     {convert(culture.contents)}
+    <Testimonials />
   </div>
+
 ))
