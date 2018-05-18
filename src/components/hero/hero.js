0e598983b28ee95c60e4f38b28b1b88c37a8162b
mtb24 <kendowney.com@gmail.com>
2018-05-18 01:48:37 -0700

    allow for passing classes in via props

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -3,7 +3,7 @@ import './hero.scss'
 
 export default props => (
 
-    <div className='hero-wrapper' style={ {backgroundImage: `url(${props.backgroundImage})`,backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center bottom'}}>
+    <div className={`hero-wrapper ${props.className || ''}`} style={ {backgroundImage: `url(${props.backgroundImage})`,backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center bottom'}}>
         <h1 className='title'>{ props.title }</h1>
     </div>
 )
