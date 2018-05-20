053927f78c4653e8ab04032835944f2695c66b16
mtb24 <kendowney.com@gmail.com>
2018-05-20 04:07:26 -0700

    use CTA component for headings

--- src/components/testimonials/testimonials.js
+++ src/components/testimonials/testimonials.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Slider from 'react-slick'
+import Cta from '../layout/page-section/cta'
 import './testimonials.scss'
 
 export default (props) => {
@@ -11,34 +12,35 @@ export default (props) => {
         slidesToScroll: 1,
     }
     return (
-        <section className='testimonial-slider'>
-            <h2>Testimonials</h2>
-            <Slider {...settings}>
-                <div>
-                    <div className='testimonial-content'>Appstem did an exceptional job building Tesla’s Marketing iPad App. We are tough customers, expecting the same excellence in our app designs and interactions as our cars, and Appstem delivered a fantastic product.</div>
-                    <div className='testimonial-author'>Nick</div>
-                    <div className='testimonial-company'>Tesla Motors</div>
-                </div>
-                <div>
-                    <div className='testimonial-content'>Appstem did an exceptional job building Tesla’s Marketing iPad App. We are tough customers, expecting the same excellence in our app designs and interactions as our cars, and Appstem delivered a fantastic product.</div>
-                    <div className='testimonial-author'>John</div>
-                    <div className='testimonial-company'>Some Company</div>
-                </div>
-                <div>
-                    <div className='testimonial-content'>Appstem did an exceptional job building Tesla’s Marketing iPad App. We are tough customers, expecting the same excellence in our app designs and interactions as our cars, and Appstem delivered a fantastic product.</div>
-                    <div className='testimonial-author'>Mike</div>
-                    <div className='testimonial-company'>Another Company</div>
-                </div>
-                <div>
-                    <div className='testimonial-content'>Appstem did an exceptional job building Tesla’s Marketing iPad App. We are tough customers, expecting the same excellence in our app designs and interactions as our cars, and Appstem delivered a fantastic product.</div>
-                    <div className='testimonial-author'>Adriana</div>
-                    <div className='testimonial-company'>ABC</div>
-                </div>
-                <div>
-                    <div className='testimonial-content'>Appstem did an exceptional job building Tesla’s Marketing iPad App. We are tough customers, expecting the same excellence in our app designs and interactions as our cars, and Appstem delivered a fantastic product.</div>
-                    <div className='testimonial-author'>Jenny</div>
-                    <div className='testimonial-company'>Sopwith Camels, Inc</div>
-                </div>
-            </Slider>
+        <section className={`testimonial-slider ${props.className || ''}`}>
+            <Cta heading='Testimonials'>
+                <Slider {...settings}>
+                    <div>
+                        <div className='testimonial-content'>Appstem did an exceptional job building Tesla’s Marketing iPad App. We are tough customers, expecting the same excellence in our app designs and interactions as our cars, and Appstem delivered a fantastic product.</div>
+                        <div className='testimonial-author'>Nick</div>
+                        <div className='testimonial-company'>Tesla Motors</div>
+                    </div>
+                    <div>
+                        <div className='testimonial-content'>Appstem did an exceptional job building Tesla’s Marketing iPad App. We are tough customers, expecting the same excellence in our app designs and interactions as our cars, and Appstem delivered a fantastic product.</div>
+                        <div className='testimonial-author'>John</div>
+                        <div className='testimonial-company'>Some Company</div>
+                    </div>
+                    <div>
+                        <div className='testimonial-content'>Appstem did an exceptional job building Tesla’s Marketing iPad App. We are tough customers, expecting the same excellence in our app designs and interactions as our cars, and Appstem delivered a fantastic product.</div>
+                        <div className='testimonial-author'>Mike</div>
+                        <div className='testimonial-company'>Another Company</div>
+                    </div>
+                    <div>
+                        <div className='testimonial-content'>Appstem did an exceptional job building Tesla’s Marketing iPad App. We are tough customers, expecting the same excellence in our app designs and interactions as our cars, and Appstem delivered a fantastic product.</div>
+                        <div className='testimonial-author'>Adriana</div>
+                        <div className='testimonial-company'>ABC</div>
+                    </div>
+                    <div>
+                        <div className='testimonial-content'>Appstem did an exceptional job building Tesla’s Marketing iPad App. We are tough customers, expecting the same excellence in our app designs and interactions as our cars, and Appstem delivered a fantastic product.</div>
+                        <div className='testimonial-author'>Jenny</div>
+                        <div className='testimonial-company'>Sopwith Camels, Inc</div>
+                    </div>
+                </Slider>
+            </Cta>
         </section>
 )}
