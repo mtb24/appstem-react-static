2e34a5114ef4c34471f956a9b765c35a9772c71a
mtb24 <kendowney.com@gmail.com>
2018-05-21 22:23:59 -0700

    enable hero to have either video or image

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,10 +1,47 @@
 import React from 'react'
+import Header from '../header/header'
+
 import './hero.scss'
 
-export default props => (
+export default props => {
 
-    <div className={`hero-wrapper ${props.className || ''}`} style={ {backgroundImage: `url(${props.backgroundImage})`,backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center bottom'}}>
-        <h1 className='title'>{ props.title }</h1>
-    </div>
-)
+    /* Outputs a hero with either an image or video background  */
+    switch( props.heroType ) {
 
+        case 'image':
+            console.log('Hero image loading...')
+            return (
+                <div className={`hero-wrapper ${props.className || ''}`} style={ {backgroundImage: `url(${props.backgroundImage})`,backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center bottom'}}>
+                    <Header/>
+                    <h1 className='title'>{ props.title || 'Default Title' }</h1>
+                </div>
+            )
+            break
+        
+        case 'video': 
+            console.log('Hero video loading...')
+            return (
+                <video
+                    mute='1'
+                    autoPlay='1'
+                    loop='1'
+                    playsInline='1'
+                    poster={`assets/images/${ props.posterImg || ''}`}
+                    className={`hero-wrapper ${props.className || ''}`}
+                    style={{
+                        maxWidth: '1400px',
+                        width: '100%',
+                    }}>
+                    <source src={ `assets/videos/${ props.backgroundVideo || '' }` }/>
+                    <Header/>
+                    <h1 className='title'>{ props.title || 'Default Title' }</h1> 
+                </video>
+            )
+            break
+        default:
+            console.error('Hero component: no heroType prop was passed!')
+            return null
+    }
+
+
+}
