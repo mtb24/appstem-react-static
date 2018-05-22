ca4ed1edddb7cf7f1c58195754fbc04e3020f500
mtb24 <kendowney.com@gmail.com>
2018-05-21 22:12:28 -0700

    add button component

---
+++ src/components/buttons/link-outlined/link-outlined.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './link-outlined.scss'
+
+export default (props) => (
+    <Link to={ props.linkTo } className={`link-outlined all-caps ${props.className || ''}`}>{ props.children }</Link>
+)
