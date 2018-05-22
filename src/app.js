e134628c4d0387b18406691fcf51acf1478e5e43
mtb24 <kendowney.com@gmail.com>
2018-05-21 22:21:26 -0700

    header moves to pages

--- src/app.js
+++ src/app.js
@@ -2,14 +2,12 @@ import React from 'react'
 import { Router, Link } from 'react-static'
 import { hot } from 'react-hot-loader'
 import Routes from 'react-static-routes'
-import Header from './components/header/header'
 import Footer from './components/footer/footer'
 import './app.scss'
 
 const App = () => (
   <Router>
     <div className='app-wrapper'>
-      <Header />
       <Routes />
       <Footer />
       <div className='page-bottom'>
