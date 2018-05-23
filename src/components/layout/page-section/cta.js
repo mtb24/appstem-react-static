10168dd2f164ed283ea163e9dd00063a001784ff
mtb24 <kendowney.com@gmail.com>
2018-05-23 11:57:16 -0700

    use div instead of section

--- src/components/layout/page-section/cta.js
+++ src/components/layout/page-section/cta.js
@@ -2,9 +2,9 @@ import React from 'react'
 import './cta.scss'
 
 export default (props) => (
-    <section className={ `cta-wrapper ${ props.className || '' }` }>
+    <div className={ `cta-wrapper ${ props.className || '' }` }>
         <h2 className='all-caps'>{ props.heading }</h2>
         <hr className='hr-green'/>
         { props.children }
-    </section>
+    </div>
 )
