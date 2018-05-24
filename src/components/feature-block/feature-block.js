9085c02a475db01b946380516c280e7722e4780f
mtb24 <kendowney.com@gmail.com>
2018-05-23 22:06:28 -0700

    replace image component

--- src/components/feature-block/feature-block.js
+++ src/components/feature-block/feature-block.js
@@ -2,23 +2,26 @@ import React from 'react'
 import convert from 'htmr'
 import { Link } from 'react-static'
 import Arrow from './arrow_white.png'
-import ImageBox from '../image-box/image-box'
+import Img from 'react-image'
+import { BeatLoader } from 'react-spinners'
 import './feature-block.scss'
 
 export default props => {
     
     if(!props.caseStudy) return null 
     else {
-        console.log('caseStudy from work page: ', props.caseStudy)
+
         const pathToImages = `assets/images/case-studies/${props.caseStudy.slug}`
 
         return (
-            <div className={ `feature-block-wrapper feature-image-${ props.imagePosition } ${ props.className }`}>
-
-                <ImageBox key={ `image-${props.caseStudy.slug}` } 
-                          image={ `${pathToImages}/medium/${props.caseStudy.imageName}` }
-                          className='feature-block-image'
-                          alt={ props.caseStudy.imageAlt || 'an image' } />
+            <div className={ `feature-block-wrapper feature-image-${ props.imagePosition } ${ props.className || '' }`}>
+            
+                <Img
+                    key={ `image-${ props.caseStudy.slug }` }
+                    className='feature-block-image'
+                    src={ `${ pathToImages }/medium/${ props.caseStudy.imageName }` }
+                    alt={ props.caseStudy.imageAlt || 'an image' }
+                    loader={ <BeatLoader color={ '#3cd52e' } loading={ true } /> } />
 
                 <div className='feature-block-details'>
 
