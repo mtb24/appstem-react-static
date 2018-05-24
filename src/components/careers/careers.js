838fb05e29f0a21b8f749678820402f119d8f693
mtb24 <kendowney.com@gmail.com>
2018-05-24 02:44:51 -0700

    add careers page

---
+++ src/components/careers/careers.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { withRouteData, Link } from 'react-static'
+import convert from 'htmr'
+import Hero from '../hero/hero'
+import backgroundImage from '../../../public/assets/images/main-pages/large/bg_careers.jpg'
+import Cta from '../layout/page-section/cta'
+
+export default withRouteData(({ work }) => {
+  
+    return (
+
+        <React.Fragment>
+
+            <Hero title={ careers.title } 
+                    heroType='image' 
+                    backgroundImage={ backgroundImage } 
+                    className='angle--bottom-right' />
+
+            <div className='careers-wrapper angle--bottom-right'>
+
+                <Cta heading='About Appstem' className='about-appstem'>
+                    <p>Our mission is to make purpose-built apps that serve real needs in people’s lives. We use metrics-driven mobile strategies, human-centered design and efficient development to create apps that people want to use.</p>
+                </Cta>
+
+                <Cta heading='Why Appstem' className='why-appstem'>
+                    {convert(careers.contents)}
+                </Cta>
+
+                <Cta heading='Open Positions' className='open-positions'>
+                    {/* accordion here */}
+                </Cta>
+
+                <p className='send-an-email'>You can’t find what you are looking for, send us an email <span>careers@appstem.com</span></p>
+
+            </div>
+
+        </React.Fragment>
+)})
