c0769dfb611cef79d71b5dc8dece8e30e099225f
mtb24 <kendowney.com@gmail.com>
2018-05-26 13:56:47 -0700

    adds careers page

--- src/components/careers/careers.js
+++ src/components/careers/careers.js
@@ -2,8 +2,12 @@ import React from 'react'
 import { withRouteData, Link } from 'react-static'
 import convert from 'htmr'
 import Hero from '../hero/hero'
-import backgroundImage from '../../../public/assets/images/main-pages/large/bg_careers.jpg'
 import Cta from '../layout/page-section/cta'
+import Collapsible from 'react-collapsible'
+import backgroundImage from '../../../public/assets/images/main-pages/large/bg_careers.jpg'
+import timeIcon from '../../../public/assets/images/main-pages/icons/ic-time@2x.png'
+import locationIcon from '../../../public/assets/images/main-pages/icons/ic-destination-outline@2x.png'
+import circleButton from '../buttons/circle-button/circle-button'
 import './careers.scss'
 
 export default withRouteData(({ careers, openings }) => {
@@ -30,7 +34,23 @@ export default withRouteData(({ careers, openings }) => {
                 <Cta heading='Open Positions' className='open-positions'>
                     {
                         /* accordion here */
-                        console.log('open positions: ', openings)
+                        //console.log('open positions: ', openings)
+                        openings.map( (job, i) => {
+                            const triggerEl = (
+                                <div className='job-opening-trigger'>
+                                    <div className='job-title all-caps'>{ job.role }</div>
+                                    <div className='job-meta'>
+                                        <div className='job-type'><img src={ timeIcon } alt='Icon for job type'/> { job.type }</div>
+                                        <div className='job-location'><img src={ locationIcon } alt='Icon for job location'/> { job.location }</div>
+                                        <div className='trigger-button'>{ circleButton }</div>
+                                    </div>
+                                </div>
+                            )
+                            return (
+                                <Collapsible key={ `job-${i}` } trigger={ triggerEl }>
+                                    { convert( job.contents ) }
+                                </Collapsible>
+                        )})
                     }
                 </Cta>
 
