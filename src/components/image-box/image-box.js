aac0cd568ade9c1aa622ca8c7052306c0fb97053
mtb24 <kendowney.com@gmail.com>
2018-05-26 13:53:52 -0700

    converted to stateless function

--- src/components/image-box/image-box.js
+++ src/components/image-box/image-box.js
@@ -1,38 +1,19 @@
 import React from 'react'
 import Img from 'react-image'
 import { BeatLoader } from 'react-spinners'
-
 import './image-box.scss'
 
-class ImageBox extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            loading: true,
-            isVisible: false
-        }
-
-    }
-    onLoadingChange(isVisible) {
-        this.setState({ isVisible: isVisible })
-        console.log('Element is now %s', isVisible ? 'visible' : 'hidden');
-    };
-
-    render() {
-
-        return !this.props.image ? null : (
-            <div className='imagebox-wrapper'>
-                <Img
-                    className={`ib-image ${ this.props.className || '' }`}
-                    src={ `assets/images/main-pages/small/${this.props.image[0]}` }
-                    alt={ this.props.image[1] }
-                    loader={ <BeatLoader color={'#3cd52e'} loading={this.state.loading} /> } />
-                <div className='ib-title'>{ this.props.image[2] || 'Title' }</div>
-                <div className='ib-desc'>{ this.props.image[3] || 'Description' }</div>
-            </div>
-        )
-    }
+export default props => {
+    
+    return !props.image ? null : (
+        <div className='imagebox-wrapper'>
+            <Img
+                className={`ib-image ${ props.className || '' }`}
+                src={ `assets/images/main-pages/small/${props.image[0]}` }
+                alt={ props.image[1] }
+                loader={ <BeatLoader color={'#3cd52e'} loading={ true } /> } />
+            <div className='ib-title'>{ props.image[2] || 'Title' }</div>
+            <div className='ib-desc'>{ props.image[3] || 'Description' }</div>
+        </div>
+    )
 }
-
-export default ImageBox
