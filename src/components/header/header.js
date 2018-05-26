d720209f467c259365e856b51ddffd1640c0b036
mtb24 <kendowney.com@gmail.com>
2018-05-25 17:44:36 -0700

    replace logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-static'
 import Nav from '../nav/nav'
-import AppstemLogo from '../social/icons/appstem-logo'
+import Img from 'react-image'
+import Logo from '../../../public/assets/images/main-pages/icons/Appstem Logo_white.png'
+import { BeatLoader } from 'react-spinners'
 import './header.scss'
 
 export default () => (
@@ -9,7 +11,12 @@ export default () => (
     <div className={ 'header-wrapper' }>
 
         <Link to="/">
-            <AppstemLogo className='header-logo' width='175' height='40' fillColor='#fefefe'/>
+
+            <Img className='header-logo'
+                 src={ Logo }
+                 alt='Appstem company logo'
+                 loader={ <BeatLoader color={'#3cd52e'} loading={ true } /> } />
+
         </Link>
 
         <Nav className='all-caps' />
