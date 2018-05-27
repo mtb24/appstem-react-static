b4551407a9625bceba0832de934fdabf501562d3
mtb24 <kendowney.com@gmail.com>
2018-05-27 02:15:52 -0700

    add className to props

--- src/components/buttons/link-outlined/link-outlined.js
+++ src/components/buttons/link-outlined/link-outlined.js
@@ -3,5 +3,5 @@ import { Link } from 'react-router-dom';
 import './link-outlined.scss'
 
 export default (props) => (
-    <Link to={ props.linkTo } className={`link-outlined all-caps ${props.className || ''}`}>{ props.children }</Link>
+    <Link to={ props.linkTo } className={`link-outlined all-caps ${props.className || ''}`}>{ props.children || null }</Link>
 )
