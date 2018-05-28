a7aff3c656e821e477a3844e1c190b0f73a268c5
mtb24 <kendowney.com@gmail.com>
2018-05-27 17:13:04 -0700

    add wrapper element

--- src/components/buttons/apply-button/apply-button.js
+++ src/components/buttons/apply-button/apply-button.js
@@ -3,5 +3,5 @@ import { Link } from 'react-static'
 import './apply-button.scss'
 
 export default props => (
-    <Link to={ props.linkTo }><button className='apply-button all-caps'>Apply</button></Link>
+    <div className='apply-button-wrapper'><Link to={ props.linkTo }><button className='apply-button all-caps'>Apply</button></Link></div>
 )
