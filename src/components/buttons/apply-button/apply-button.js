b45210c69db8379b0ba9bc0c7b4097aa2e7d15d0
mtb24 <kendowney.com@gmail.com>
2018-05-29 01:23:20 -0700

    format code

--- src/components/buttons/apply-button/apply-button.js
+++ src/components/buttons/apply-button/apply-button.js
@@ -3,5 +3,9 @@ import { Link } from 'react-static'
 import './apply-button.scss'
 
 export default props => (
-    <div className='apply-button-wrapper'><Link to={ props.linkTo }><button className='apply-button all-caps'>Apply</button></Link></div>
+    <div className='apply-button-wrapper'>
+        <Link to={ props.linkTo }>
+            <button className='apply-button all-caps'>Apply</button>
+        </Link>
+    </div>
 )
