ff43ed2daf0e06db5fdd94f3b284acae4b41531b
mtb24 <kendowney.com@gmail.com>
2018-06-08 01:38:16 -0700

    add wrapper and styles

--- src/components/careers/careers.js
+++ src/components/careers/careers.js
@@ -10,6 +10,7 @@ import locationIcon from '../../../public/assets/images/main-pages/icons/ic-dest
 import openButton from '../../../public/assets/images/icons/chevron-right.svg'
 import closeButton from '../../../public/assets/images/icons/circle-outline-with-x.svg'
 import Button from '../buttons/base-button/button'
+import AngledSection from '../layout/page-section/angled-section'
 import './careers.scss'
 
 export default withRouteData(({ careers, openings }) => {
@@ -26,45 +27,47 @@ export default withRouteData(({ careers, openings }) => {
                     backgroundImage={ backgroundImage } 
                     className='angle--bottom-right' />
 
-            <div className='careers-wrapper angle--both-left-right'>
+            <AngledSection className='careers-wrapper' mask='angle--both-left-right'>
+                <div className='careers content-wrapper'>
 
-                <Cta heading='About Appstem' className='about-appstem'>
-                    <p>Our mission is to make purpose-built apps that serve real needs in people’s lives. We use metrics-driven mobile strategies, human-centered design and efficient development to create apps that people want to use.</p>
-                </Cta>
+                    <Cta heading='About Appstem' className='about-appstem'>
+                        <p>Our mission is to make purpose-built apps that serve real needs in people’s lives. We use metrics-driven mobile strategies, human-centered design and efficient development to create apps that people want to use.</p>
+                    </Cta>
 
-                <Cta heading='Why Appstem' className='why-appstem'>
-                    {convert(careers.contents)}
-                </Cta>
+                    <Cta heading='Why Appstem' className='why-appstem'>
+                        {convert(careers.contents)}
+                    </Cta>
 
-                <Cta heading='Open Positions' className='open-positions'>
-                    {
-                        openings.map( (job, i) => {
-                            const triggerEl = (
-                                <div className='job-opening-trigger'>
-                                    <div className='job-title all-caps'>{ job.role }</div>
-                                    <div className='job-meta'>
-                                        <div className='job-type'><img src={ timeIcon } alt='Icon for job type'/> { job.type }</div>
-                                        <div className='job-location'><img src={ locationIcon } alt='Icon for job location'/> { job.location }</div>
-                                        { open }
+                    <Cta heading='Open Positions' className='open-positions'>
+                        {
+                            openings.map( (job, i) => {
+                                const triggerEl = (
+                                    <div className='job-opening-trigger'>
+                                        <div className='job-title all-caps'>{ job.role }</div>
+                                        <div className='job-meta'>
+                                            <div className='job-type'><img src={ timeIcon } alt='Icon for job type'/> { job.type }</div>
+                                            <div className='job-location'><img src={ locationIcon } alt='Icon for job location'/> { job.location }</div>
+                                            { open }
+                                        </div>
                                     </div>
-                                </div>
-                            )
-                            return (
-                                <React.Fragment>
-                                    <Collapsible key={ i } trigger={ triggerEl } triggerOpenedClassName='rotate' >
-                                        { convert( job.contents ) }
-                                        <Button linkTo='#' buttonText='Apply' className='apply-button'/>
-                                    </Collapsible>
-                                    { i < openings.length - 1 ? <hr className='job-opening-seperator'/> : null }
-                                </React.Fragment>
-                            )
-                        })
-                    }
-                </Cta>
+                                )
+                                return (
+                                    <React.Fragment>
+                                        <Collapsible key={ i } trigger={ triggerEl } triggerOpenedClassName='rotate' >
+                                            { convert( job.contents ) }
+                                            <Button linkTo='#' buttonText='Apply' className='apply-button'/>
+                                        </Collapsible>
+                                        { i < openings.length - 1 ? <hr className='job-opening-seperator'/> : null }
+                                    </React.Fragment>
+                                )
+                            })
+                        }
+                    </Cta>
 
-                <p className='send-an-email'>You can’t find what you are looking for, send us an email <br/><span>careers@appstem.com</span></p>
+                    <p className='send-an-email'>You can’t find what you are looking for, send us an email <br/><span>careers@appstem.com</span></p>
 
-            </div>
+                </div>
+            </AngledSection>
 
         </React.Fragment>
 )})
