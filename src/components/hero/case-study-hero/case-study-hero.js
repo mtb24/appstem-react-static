d9aed9ac72dbf1de99434a8bd60f786bf1464a98
mtb24 <kendowney.com@gmail.com>
2018-06-11 16:20:00 -0700

    add default value in case no prop

--- src/components/hero/case-study-hero/case-study-hero.js
+++ src/components/hero/case-study-hero/case-study-hero.js
@@ -9,7 +9,7 @@ export default (props) => {
                 className={`hero case-study-hero ${props.className || ''}`} 
                 style={ 
                     {
-                        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(${props.backgroundImage})`,
+                        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(${props.backgroundImage || ''})`,
                         backgroundRepeat: 'no-repeat', 
                         backgroundSize: 'cover', 
                         backgroundPosition: 'center center'
