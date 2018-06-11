c144fd83f97541b3527392e88ea1e6b0260f1a39
mtb24 <kendowney.com@gmail.com>
2018-06-11 01:35:23 -0700

    add hero components

---
+++ src/components/hero/case-study-hero/case-study-hero.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './case-study-hero.scss'
+
+export default (props) => {
+
+    return (
+        <React.Fragment>
+            <div 
+                className={`hero case-study-hero ${props.className || ''}`} 
+                style={ 
+                    {
+                        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(${props.backgroundImage})`,
+                        backgroundRepeat: 'no-repeat', 
+                        backgroundSize: 'cover', 
+                        backgroundPosition: 'center center'
+                    }
+                }>
+                <h1 className='title'>{ props.title || 'Default Title' }</h1>
+            </div>
+        </React.Fragment>
+    )
+}
---
+++ src/components/hero/video-hero/video-hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './video-hero.scss'
+
+export default (props) => (
+    <React.Fragment>
+    <div className={`hero video-hero ${props.className || ''}`}>
+        <span className='video-title'>{ props.title || 'Default Title' }</span>
+        <video
+            mute='1'
+            autoPlay='1'
+            loop='1'
+            playsInline='1'
+            poster={`assets/images/${ props.posterImg || ''}`}>
+            <source src={ `assets/videos/${ props.backgroundVideo || '' }` }/>
+        </video>
+    </div>
+    </React.Fragment>
+)
