fc29d9eef1093a6ff1591ef6030aab246ad6d7bd
mtb24 <kendowney.com@gmail.com>
2018-06-14 00:55:59 -0700

    add Link component

--- src/components/nav/section-links/section-links.js
+++ src/components/nav/section-links/section-links.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-static'
 import './section-links.scss'
 import leftArrow from '../../../../public/assets/images/icons/left-arrow.png'
 import rightArrow from '../../../../public/assets/images/icons/right-arrow.png'
@@ -12,11 +13,19 @@ export default (props) => {
     
     <div className={`section-links ${props.className || ''}`}>
 
-        <div className={`${alignclass} all-caps`}><img src={ leftArrow } alt='Left arrow icon'/> { props.leftText || 'Previous' }</div>
+        <Link to=''>
+            <div className={`${alignclass} sl-left all-caps`}>
+                <img src={ leftArrow } alt='Left arrow icon'/> { props.leftText || 'Previous' }
+            </div>
+        </Link>
 
         { props.centerText? <LinkOutlined linkTo='/work' className=''>{ props.centerText }</LinkOutlined> : null }
 
-        <div className={`${alignclass} all-caps`}>{ props.rightText || 'Next' } <img src={ rightArrow } alt='Right arrow icon'/></div>
+        <Link to=''>
+            <div className={`${alignclass} sl-right all-caps`}>
+                { props.rightText || 'Next' } <img src={ rightArrow } alt='Right arrow icon'/>
+            </div>
+        </Link>
         
     </div>
 
