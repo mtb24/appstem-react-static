c3c9944c8445d3d4a3e0c3d97f541c729c5169aa
mtb24 <kendowney.com@gmail.com>
2018-06-15 22:27:06 -0700

    change class order

--- src/components/layout/page-section/heading.js
+++ src/components/layout/page-section/heading.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default (props) => {
     const Heading = `h${props.headingLevel || '2'}`
     return (
-        <Heading className={`all-caps ${ props.className }`}>{props.children || 'Default Heading'}</Heading>
+        <Heading className={`all-caps ${ props.className || '' }`}>{props.children || 'Default Heading'}</Heading>
     )
 }
