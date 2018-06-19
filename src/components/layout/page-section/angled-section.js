942c2bd5d32a19a9c1521b9dca1623c06635f2dc
mtb24 <kendowney.com@gmail.com>
2018-06-19 15:48:04 -0700

    add styles inline

--- src/components/layout/page-section/angled-section.js
+++ src/components/layout/page-section/angled-section.js
@@ -4,9 +4,16 @@ import './angled-section.scss'
 export default props => {
 
     const mask = props.mask || 'angle--bottom-right';
-    
+    const defaultStyle = {
+        display: 'flex',
+        width: '100vw',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: '0 auto',
+    }
+
     return (
-        <div className={ `angled-section ${ mask } ${ props.className || '' }` }>
+        <div className={ `angled-section ${ mask } ${ props.className || '' }` } style={ defaultStyle }>
             { props.children }
         </div>
     )
