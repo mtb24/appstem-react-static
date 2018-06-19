294eba45ff514834d29f9ae9666a4c660e6268e3
mtb24 <kendowney.com@gmail.com>
2018-06-19 15:49:00 -0700

    scratchpad for modal

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -2,11 +2,31 @@ import React from 'react'
 import ConfirmationModal from '../confirmationModal/confirmation-modal'
 import './blog.scss'
 
-export default (props) => {
-  console.log('props: ', props)
-  return (
-        <div className='blog'>
-            <ConfirmationModal buttonText='Ok'/>
+class Blog extends React.Component {
+    constructor(props) {
+      super(props)
+  
+      this.state = { isOpen: false }
+    }
+  
+    toggleModal = () => {
+      this.setState({
+        isOpen: !this.state.isOpen
+      })
+    }
+  
+    render() {
+      return (
+        <div className="blog">
+          <button onClick={this.toggleModal}>
+            Open the modal
+          </button>
+  
+          <ConfirmationModal buttonText='Ok' show={this.state.isOpen} onClose={this.toggleModal} className={ this.state.isOpen === true ? 'is-open' : 'is-closed' } />
+
         </div>
-    )
-}
+      );
+    }
+  }
+  
+  export default Blog;
--- src/components/confirmationModal/confirmation-modal.js
+++ src/components/confirmationModal/confirmation-modal.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import Cta from '../layout/page-section/cta'
-import Button from '../buttons/base-button/button'
 import './confirmation-modal.scss'
 
-export default (props) => (
-    <div className='confirmation-wrapper'>
-        <Cta heading='Thank You!'>
-            <p>Your message has been sent successfully. We will be in contact soon.</p>
-            <Button linkTo='#' buttonText={ props.buttonText }/>
-        </Cta>
-    </div>
-)
+
+class ConfirmationModal extends React.Component {
+  render() {
+    // Render nothing if the "show" prop is false
+    if(!this.props.show) {
+      return null
+    }
+
+    return (
+      <div className={`backdrop ${ this.props.className || '' }`}>
+        <div className={`modal ${ this.props.className || '' }`}>
+            <Cta heading='Thank You!'>
+                <p>Your message has been sent successfully. We will be in contact soon.</p>
+                <button onClick={this.props.onClose} className='all-caps'>{ this.props.buttonText }</button>
+            </Cta>
+        </div>
+      </div>
+    );
+  }
+}
+
+ConfirmationModal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  show: PropTypes.bool,
+  children: PropTypes.node
+}
+
+export default ConfirmationModal
