4de42d45d6f7b18685c1166ca1c31e4c0314aad4
mtb24 <kendowney.com@gmail.com>
2018-06-20 14:23:21 -0700

    remove comments

--- src/index.js
+++ src/index.js
@@ -7,13 +7,12 @@ import App from './app'
 // Export your top level component as JSX (for static rendering)
 export default App
 
-// Render your app
+
 if (typeof document !== 'undefined') {
   const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render
   const render = Comp => {
     renderMethod(<Comp />, document.getElementById('root'))
   }
 
-  // Render!
   render(App)
 }
