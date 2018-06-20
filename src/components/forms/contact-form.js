a3de073156fec9024e02e3150446a2c2f8eb9d70
mtb24 <kendowney.com@gmail.com>
2018-06-20 14:23:52 -0700

    add form handling and confirmation

--- src/components/forms/contact-form.js
+++ src/components/forms/contact-form.js
@@ -1,69 +1,162 @@
 import React from 'react'
-import { Form, Text } from 'react-form'
+import ReactModal from 'react-modal'
+//import { Form, Text } from 'react-form'
 import { ClipLoader } from 'react-spinners'
-import ConfirmationModal from '../confirmationModal/confirmation-modal'
 import Button from '../buttons/base-button/button'
+import Cta from '../layout/page-section/cta'
 
+import '../confirmationModal/confirmation-modal.scss'
 
-const validate = value => ({
+class ContactForm extends React.Component {
+
+    constructor (props) {
+          super(props)
   
-    // TODO: validation is not complete
+          this.state = {
+          type: 'info',
+          message: '',
+          loading: false,
+          status: '',
+          showModal: false,
+          }
+  
+          this.handleSubmit = this.handleSubmit.bind(this)
+          this.sendFormData = this.sendFormData.bind(this)
+          this.requestBuildQueryString = this.requestBuildQueryString.bind(this)
+          this.getFormData = this.getFormData.bind(this)
+          this.handleOpenModal = this.handleOpenModal.bind(this)
+          this.handleCloseModal = this.handleCloseModal.bind(this)
+          this.toggleModal = this.toggleModal.bind(this)
+    }
+
+    handleOpenModal () {
+        console.log('handleOpenModal fired')
+        this.setState({ showModal: true })
+    }
     
-    error: !value || !/Hello World/.test(value) ? "Input must contain 'Hello World'" : null,
-    warning: !value || !/^Hello World$/.test(value) ? "Input should equal just 'Hello World'" : null,
-    success: value && /Hello World/.test(value) ? "Thanks for entering 'Hello World'!" : null
-  })
+    handleCloseModal () {
+        this.setState({ showModal: false })
+    }
+
+    toggleModal () {
+        this.setState({ showModal: !this.state.showModal })
+    }
   
-  const handleFormSubmit = (values, e, formApi) => {
-    console.log('submitted values: ', values)
+    handleSubmit (event) {
+      event.preventDefault()
+      this.setState({ type: 'info', message: 'sending data...', loading: true })
+      this.getFormData()
+    }
+  
+    getFormData () {
+      const formData = {
+        name: this.refs.name.value,
+        email: this.refs.email.value,
+        phone: this.refs.phone.value,
+        company: this.refs.company.value,
+        project: this.refs.project.value,
+      }
+      
+      // simulate submission for testing...
+      setTimeout( () => {
+        console.log('form data: ', formData)
+        this.setState({ 
+            type: 'success', 
+            message: 'Success', 
+            loading: false, 
+            status: 'sent', 
+            data: formData,
+            showModal: true,
+         })
+        document.getElementById('contact-form').reset()
+        // this.sendFormData()
+      }, 2000 )
+    }
+
+    sendFormData () {
+      const xmlhttp = new XMLHttpRequest()
+    //   const _this = this
+      xmlhttp.onreadystatechange = () => {
+        if (xmlhttp.readyState === 4) {
+          const response = JSON.parse(xmlhttp.responseText)
+          if (xmlhttp.status === 200 && response.status === 'OK') {
+            this.setState({ showModal: true, type: 'success', message: 'We have received your message and will get in touch shortly. Thanks!' })
+          }
+          else {
+            this.setState({ type: 'error', message: 'Sorry, there has been an error' })
+          }
+        }
+      }
+      xmlhttp.open('POST', '/some-endpoint', true)
+      xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+      xmlhttp.send(this.requestBuildQueryString(this.state.data))
+    }
+    /**
+     * Transforms an object into a URL querystring.
+     *
+     * @param object params
+     * @return string the formatted querystring.
+     */
+    requestBuildQueryString (params) {
+      const queryString = []
+      for(let property in params)
+        if (params.hasOwnProperty(property)) {
+          queryString.push(encodeURIComponent(property) + '=' + encodeURIComponent(params[property]))
+        }
+      return queryString.join('&')
+    }
+  
+    render () {
+
+      const Status = () => (<div id="status" className={'alert alert-' + this.state.type} ref="status">{this.state.message}</div>)
+      return (
+        <React.Fragment>
+          <Status/>
+          <form ref='contactform' onSubmit={this.handleSubmit} id="contact-form" className="contact-form" >
+              <div className='contact-form-field-wrapper'>
+              <input type='text' ref='name' field="name" id="name" name='name' />
+              <label className='all-caps' htmlFor="name">Name</label>
+              </div>
+  
+              <div className='contact-form-field-wrapper'>
+              <input type='text' ref='email' field="email" id="email" name='email' />
+              <label className='all-caps' htmlFor="email">Email</label>
+              </div>
+  
+              <div className='contact-form-field-wrapper'>
+              <input type='text' ref='phone' field="phone" id="phone" name='phone' />
+              <label className='all-caps' htmlFor="phone">Phone</label>
+              </div>
+  
+              <div className='contact-form-field-wrapper'>
+              <input type='text' ref='company' field="company" id="company" name='company' />
+              <label className='all-caps' htmlFor="company">Company/Organization</label>
+              </div>
+  
+              <div className='contact-form-field-wrapper'>
+              <input type='text' ref='project' field="project" id="project" name='project' />
+              <label className='all-caps' htmlFor="project">Tell us about your project</label>
+              </div>
+  
+              <Button type='submit' className='contact-form-submit' buttonText={ this.state.loading === false ? 'Send' : <ClipLoader color={'#ffffff'} loading={ this.state.loading } /> }/>
+
+          </form>
+          <ReactModal 
+                isOpen={ this.state.showModal }
+                contentLabel='confirmation - email form sucessfully submitted'
+                onRequestClose={ this.handleCloseModal }
+                className='modal'
+                overlayClassName='backdrop'>
+
+                <Cta heading='Thank You!'>
+                    <p>Your message has been sent successfully. We will be in contact soon.</p>
+                    <button onClick={ this.handleCloseModal } className='all-caps'>Ok</button>
+                </Cta>
+
+            </ReactModal>
+        </React.Fragment>
+      )
+    }
   }
   
-  // const checkForValue = inputEl => {
-  //   console.log('el changed: ', inputEl)
-  //   if (inputEl) {
-  //     if(inputEl.value) {
-  //       inputEl.classList.add('has-value')
-  //     } else {
-  //       inputEl.classList.remove('has-value')
-  //     }
-  //   }
-  // }
-
-export const ContactForm = props => (
-
-    <Form onSubmit={ submittedValues => handleFormSubmit(submittedValues) }>
-        {
-            formApi => (
-                <form action='#' id="contact-form" className="contact-form" >
-
-                    <div className='contact-form-field-wrapper'>
-                    <Text field="name" id="name" name='name' validate={validate} />
-                    <label className='all-caps' htmlFor="name">Name</label>
-                    </div>
-                    
-                    <div className='contact-form-field-wrapper'>
-                    <Text field="email" id="email" name='email' validate={validate} />
-                    <label className='all-caps' htmlFor="email">Email</label>
-                    </div>
-                    
-                    <div className='contact-form-field-wrapper'>
-                    <Text field="phone" id="phone" name='phone' validate={validate} />
-                    <label className='all-caps' htmlFor="phone">Phone</label>
-                    </div>
-                    
-                    <div className='contact-form-field-wrapper'>
-                    <Text field="company" id="company" name='company' validate={validate} />
-                    <label className='all-caps' htmlFor="company">Company/Organization</label>
-                    </div>
-                    
-                    <div className='contact-form-field-wrapper'>
-                    <Text field="project" id="project" name='project' validate={validate} />
-                    <label className='all-caps' htmlFor="project">Tell us about your project</label>
-                    </div>
-
-                    <Button linkTo='#' className="contact-form-submit" buttonText='Send'/>
-                </form>
-            )
-        }
-    </Form>
-)
+  export default ContactForm
