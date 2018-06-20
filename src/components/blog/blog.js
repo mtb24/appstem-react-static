cf78708ba5f4820f839350187ca6778de14a0273
mtb24 <kendowney.com@gmail.com>
2018-06-20 14:27:00 -0700

    remove test component

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -1,32 +1,14 @@
 import React from 'react'
-import ConfirmationModal from '../confirmationModal/confirmation-modal'
 import './blog.scss'
 
 class Blog extends React.Component {
-    constructor(props) {
-      super(props)
-  
-      this.state = { isOpen: false }
-    }
-  
-    toggleModal = () => {
-      this.setState({
-        isOpen: !this.state.isOpen
-      })
-    }
-  
-    render() {
-      return (
-        <div className="blog">
-          <button onClick={this.toggleModal}>
-            Open the modal
-          </button>
-  
-          <ConfirmationModal buttonText='Ok' show={this.state.isOpen} onClose={this.toggleModal} className={ this.state.isOpen === true ? 'is-open' : 'is-closed' } />
+    
 
-        </div>
-      );
+    render() {
+        return (
+            <div className="blog"></div>
+        )
     }
-  }
+}
   
   export default Blog;
