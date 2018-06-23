80a005270e2265876f3087ffdecfa296c26d5698
mtb24 <kendowney.com@gmail.com>
2018-06-23 09:16:46 -0700

    add animations

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,17 +6,88 @@ import Logo from '../../../public/assets/images/icons/Appstem-Logo_green.png'
 import { BeatLoader } from 'react-spinners'
 import './header.scss'
 
-export default () => (
 
-    <div className={ 'header' }>
-            <Link to="/">
-                <Img className='header-logo'
-                    src={ Logo }
-                    alt='Appstem company logo'
-                    loader={ <BeatLoader color={'#3cd52e'} loading={ true } /> } />
-            </Link>
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            showHeader: 'initial',
+            ticking: false,
+            targetHeight: 344,
+        }
+        this.handleScroll = this.handleScroll.bind(this)
+        this.calculateScroll = this.calculateScroll.bind(this)
+        this.handleNavClicks = this.handleNavClicks.bind(this)
+        this.closeNav = this.closeNav.bind(this)
+    }
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll.bind(this))
+        document.getElementsByClassName('nav-toggle').length > 0 &&
+        document.getElementsByClassName('nav-toggle')[0].addEventListener('click', this.handleNavClicks)
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll)
+        document.getElementsByClassName('nav-toggle')[0].removeEventListener('click', this.handleNavClicks)
+    }
+    handleNavClicks() {
+        const mobileNavIsOpen = document.getElementsByClassName('nav-toggle')[0].checked || ''
+        if (mobileNavIsOpen) {
+            const navLinks = document.querySelectorAll('.main-header a')
+            navLinks.forEach((linkEl) => {linkEl.addEventListener('click', this.closeNav)})
+        }
+    }
+    closeNav() {
+        document.getElementsByClassName('nav-toggle')[0].checked = false
+    }
+    handleScroll() {
+        window.requestAnimationFrame(this.calculateScroll)
+    }
+    calculateScroll() {
+        if (!this.state.ticking) {
+            if (window.scrollY > this.state.targetHeight) {
+                this.setState({
+                    showHeader: 'leave',
+                });
+            } else {
+                this.setState({
+                    showHeader: 'initial',
+                })
+            }
+            this.setState({
+                ticking: false,
+            })
+        } else {
+            this.setState({
+            ticking: true,
+        })}
+    }
 
-            <Nav className='all-caps' />
-    </div>
+    render() {
+        
+        return (
 
-);
+            <header ref='header' className={`main-header header-${this.state.showHeader}`}>
+                <div className='header-max'>
+
+                    <Link to="/">
+                        <Img className='header-logo'
+                            src={ Logo }
+                            alt='Appstem company logo'
+                            loader={ <BeatLoader color={'#3cd52e'} loading={ true } /> } />
+                    </Link>
+
+                    <label htmlFor='nav-toggle' className='nav-toggle-label'><span></span></label>
+                    <input type='checkbox' id='nav-toggle' className='nav-toggle'/>
+                    
+                    <Nav className='header-nav' />
+
+                </div>
+            </header>
+
+        )
+        
+    }
+
+}
+export default Header
