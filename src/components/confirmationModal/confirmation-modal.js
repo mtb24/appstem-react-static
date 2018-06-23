9b47f02aace4fff1dfb7a713c41a895eca99230d
mtb24 <kendowney.com@gmail.com>
2018-06-23 13:08:17 -0700

    slight refactor

--- src/components/confirmationModal/confirmation-modal.js
+++ src/components/confirmationModal/confirmation-modal.js
@@ -1,33 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import ReactModal from 'react-modal'
 import Cta from '../layout/page-section/cta'
 import './confirmation-modal.scss'
 
+ReactModal.setAppElement('#root')
 
 class ConfirmationModal extends React.Component {
-  render() {
-    // Render nothing if the "show" prop is false
-    if(!this.props.show) {
-      return null
+    constructor (props) {
+        super(props)
+        this.state = {
+            showModal: false
+        }
+        
+        this.handleOpenModal = this.handleOpenModal.bind(this)
+        this.handleCloseModal = this.handleCloseModal.bind(this)
+        this.toggleModal = this.toggleModal.bind(this)
+    }
+    
+    handleOpenModal () {
+        this.setState({ showModal: true })
+    }
+    
+    handleCloseModal () {
+        this.setState({ showModal: false })
     }
 
-    return (
-      <div className={`backdrop ${ this.props.className || '' }`}>
-        <div className={`modal ${ this.props.className || '' }`}>
-            <Cta heading='Thank You!'>
-                <p>Your message has been sent successfully. We will be in contact soon.</p>
-                <button onClick={this.props.onClose} className='all-caps'>{ this.props.buttonText }</button>
-            </Cta>
-        </div>
-      </div>
-    );
-  }
-}
+    toggleModal () {
+        this.setState({ showModal: !this.state.showModal })
+    }
+    
+    render() {
+        return (
+            <React.Fragment>                
+                <ReactModal 
+                    isOpen={this.props.show}
+                    contentLabel='confirmation - email form sucessfully submitted'
+                    onRequestClose={this.handleCloseModal}
+                    className='modal'
+                    overlayClassName='backdrop'>
 
-ConfirmationModal.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  show: PropTypes.bool,
-  children: PropTypes.node
-}
+                    <Cta heading='Thank You!'>
+                        <p>Your message has been sent successfully. We will be in contact soon.</p>
+                        <button onClick={this.handleCloseModal} className='all-caps'>Ok</button>
+                    </Cta>
 
+                </ReactModal>
+            </React.Fragment>
+        )
+    }
+}
 export default ConfirmationModal
