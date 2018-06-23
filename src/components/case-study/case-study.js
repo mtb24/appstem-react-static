017e1bd9cffd80233e596b06640cca859745be28
mtb24 <kendowney.com@gmail.com>
2018-06-23 13:07:09 -0700

    slight refactor

--- src/components/case-study/case-study.js
+++ src/components/case-study/case-study.js
@@ -7,34 +7,33 @@ import AngledSection from '../layout/page-section/angled-section'
 import './case-study.scss'
 
 export default withRouteData(({ caseStudy, caseStudiesForNav }) => {
-  // console.log('caseStudiesForNav: ',caseStudiesForNav)
-  // console.log('props: ',props)
-    // const linkList = props.linkList || []
-    const currentPage = caseStudy.slug || ''
+
+    const currentPage = caseStudy.slug !== 'undefined' ? caseStudy.slug : ''
     let prevLink = {}
     let nextLink = {}
-    const currentIndex = caseStudiesForNav.find(function(value, index) {
-        if(value.slug === currentPage) {
-            // console.log('current index ' + index + ' -> ' + value.slug + ' -> ' + value.title)
-            prevLink = caseStudiesForNav[index - 1] || caseStudiesForNav[ caseStudiesForNav.length - 1 ]
-            nextLink = caseStudiesForNav[index + 1] || caseStudiesForNav[ 0 ]
-            return index
-        }
+    const currentIndex = caseStudiesForNav.find( (value, index) => {
+      // console.log('currentPage: ', currentPage)
+      // console.log('current item: ' + index + ' -> ' + value.slug + ' -> ' + value.title)
+      if(value.slug && value.slug === currentPage) {
+        // console.log('CURRENT SLUG MATCH! ' + index + ' -> ' + value.slug + ' -> ' + currentPage)
+        prevLink = caseStudiesForNav[index - 1] || caseStudiesForNav[ caseStudiesForNav.length - 1 ]
+        nextLink = caseStudiesForNav[index + 1] || caseStudiesForNav[ 0 ]
+        // console.log('CURRENT INDEX: ', {index, prevLink, nextLink})
+        return index
+      }
     })
-    // console.log('prevLink: ',prevLink)
-    // console.log('nextLink: ',nextLink)
-    //console.log('currentIndex: ',currentIndex)
+
     const FailedToLoad = () => (
       <div style={{ color: 'red', padding: '2em' }} className='error'>
-        <h1 style={{ left: '33%', position: 'relative' }}>Failed to load the {`${caseStudy.slug || 'ANY' }`} component!</h1>
+        <h1 style={{ left: '33%', position: 'relative' }}>Failed to load the {`${currentPage || 'ANY' }`} component!</h1>
       </div>
     )
     const Loading = () => (
       <div style={{ color: '#3cd52e', padding: '2em' }} className='loading-component'>
-        <h1 style={{ left: '33%', position: 'relative' }}>Loading the {`${caseStudy.slug}`} component...</h1>
+        <h1 style={{ left: '33%', position: 'relative' }}>Loading the {`${currentPage}`} component...</h1>
       </div>
     )
-    const CaseStudyTemplate = universal(import(`./studies/${caseStudy.slug}/${caseStudy.slug}`), 
+    const CaseStudyTemplate = universal(import(`./studies/${currentPage}/${currentPage}`), 
     {
       loading: Loading,
       error: FailedToLoad,
