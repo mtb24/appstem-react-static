597f17a4e3213a525327877d4e13c15a4f280299
mtb24 <kendowney.com@gmail.com>
2018-06-23 18:50:45 -0700

    testing new InstantImage component

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import InstantImage from '../instant-image/instant-image'
 import './blog.scss'
 
 class Blog extends React.Component {
-    
 
     render() {
+        const imageFullPath = '/assets/images/case-studies/draftmates/DM.png'
+        const defaultImageFullPath = '/fav_appstem_32x32.png'
+
         return (
-            <div className="blog">Hi</div>
+            <React.Fragment>
+                <div className="blog">
+
+                <h1>Hi</h1>
+
+                <InstantImage imageSrcs={ [imageFullPath,defaultImageFullPath] } alt='Hi K2!!!' />
+
+                </div>
+            </React.Fragment>
         )
     }
 }
