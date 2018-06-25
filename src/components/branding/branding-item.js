edb1ecae6e5571c99f2c92293674800df0525e50
mtb24 <kendowney.com@gmail.com>
2018-06-24 22:10:12 -0700

    add wrapper element

--- src/components/branding/branding-item.js
+++ src/components/branding/branding-item.js
@@ -4,5 +4,9 @@ import InstantImage from '../instant-image/instant-image'
 export default (props) => {
     // console.log(props)
     const pathToImages = `/assets/images/case-studies/${ props.slug }/`
-    return props.images.map( (image, i) => <InstantImage key={i} className='branding-image' imageSrcs={ pathToImages + image || '' } alt={ `an image for ${ props.slug }` } /> )
+    return (
+        <div className='branding-images'>
+        { props.images.map( (image, i) => <InstantImage key={i} className='branding-image' imageSrcs={ pathToImages + image || '' } alt={ `an image for ${ props.slug }` } /> )}
+        </div>
+    )
 }
