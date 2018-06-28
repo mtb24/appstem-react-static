f60095e3ad41c136fab64c5fc3b2cfb17baa08b5
mtb24 <kendowney.com@gmail.com>
2018-06-28 00:14:52 -0700

    add new job opening component

--- src/components/careers/careers.js
+++ src/components/careers/careers.js
@@ -1,24 +1,15 @@
 import React from 'react'
-import { withRouteData, Link } from 'react-static'
-import convert from 'htmr'
+import { withRouteData } from 'react-static'
 import Hero from '../hero/hero'
 import Cta from '../layout/page-section/cta'
-import Collapsible from 'react-collapsible'
 import backgroundImage from '../../../public/assets/images/main-pages/bg_careers.jpg'
-import timeIcon from '../../../public/assets/images/icons/ic-time@2x.png'
-import locationIcon from '../../../public/assets/images/icons/ic-destination-outline@2x.png'
-//import openButton from '../../../public/assets/images/icons/circle-outline-with-plus-sign.svg'
-//import closeButton from '../../../public/assets/images/icons/circle-outline-with-dash.svg'
-import Button from '../buttons/base-button/button'
 import AngledSection from '../layout/page-section/angled-section'
+import JobOpening from '../job-opening/job-opening'
 import './careers.scss'
 
-export default withRouteData(({ careers, openings }) => {
-
-    //const open = <img className='trigger-button' alt='circle icon to open panel' src={ openButton }/>
-    //const close = <img className='trigger-button' alt='circle icon to close panel' src={ closeButton }/>
-    const button = <span className='trigger-button' alt='circle icon to open and close panel'></span>
-  
+export default withRouteData(({ careers, jobs }) => {
+    // console.log('careers: ',careers)
+    // console.log('jobs: ',jobs)
     return (
 
         <React.Fragment>
@@ -36,32 +27,33 @@ export default withRouteData(({ careers, openings }) => {
                     </Cta>
 
                     <Cta heading='Why Appstem' className='why-appstem'>
-                        {convert(careers.contents)}
+                        <ul>
+                            <li>Competitive salary</li>
+                            <li>Work with a smart, driven, fun group of people</li>
+                            <li>Health, Dental, Vision Benefits</li>
+                            <li>401K</li>
+                            <li>Flexible and fun work environment (work from home Fridays)</li>
+                            <li>Employees first (we will do everything in our power to keep you happy!)</li>
+                            <li>Occasional get-togethers including sporting events, happy hours, dinners, group events and more!</li>
+                        </ul>
+                            
+                        <p>To be considered, applicants must:</p>
+                        <ul>
+                            <li>Be authorized to work in the United States on a full-time basis for any employer</li>
+                            <li>Must be able to commute to San Francisco </li>
+                            <li>Have great English communication skills</li>
+                            <li>We are NOT interested in working with any recruiters or third party companies of any kind</li>
+                        </ul>
                     </Cta>
 
                     <Cta heading='Open Positions' className='open-positions'>
                         {
-                            openings.map( (job, i) => {
-                                const TriggerEl = (
-                                    <div key={ `${i}-trigger` } className='job-opening-trigger'>
-                                        <div className='job-title all-caps'>{ job.role }</div>
-                                        <div className='job-meta'>
-                                            <div className='job-type'><img src={ timeIcon } alt='Icon for job type'/> { job.type }</div>
-                                            <div className='job-location'><img src={ locationIcon } alt='Icon for job location'/> { job.location }</div>
-                                            { button }
-                                        </div>
-                                    </div>
-                                )
-                                return (
-                                    <React.Fragment key={i}>
-                                        <Collapsible key={i} trigger={ TriggerEl } triggerOpenedClassName='opened' >
-                                            { convert( job.contents ) }
-                                            <Button linkTo='#' buttonText='Apply' className='apply-button'/>
-                                        </Collapsible>
-                                        { i < openings.length - 1 ? <hr className='job-opening-seperator'/> : null }
-                                    </React.Fragment>
-                                )
-                            })
+                            jobs.map( (job, i) => (
+                                <React.Fragment key={i}>
+                                    <JobOpening job={ job } />
+                                    { i < jobs.length - 1 ? <hr className='job-opening-seperator'/> : null }
+                                </React.Fragment>
+                            ))
                         }
                     </Cta>
 
